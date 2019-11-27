90324f26fd10b3bb478f5d7fcb01f5c0180db2df
peterkabaijr <56918289+peterkabaijr@users.noreply.github.com>
2019-11-27 03:53:44 -0800

    Add files via upload

---
+++ Group project.py
@@ -0,0 +1,109 @@
+arts_questions = {
+                         '''The character matched against the hero of a play is the?
+                            a) Antagonist
+                            b) Protagonist
+                            c) Clown
+                            d) Round Character''',
+                         '''He bleats like a goat is a example of?
+                            a) Simile
+                            b) Hyperbole
+                            c) Metaphor
+                            d) Euphemism''',
+                         '''A simple story with a deeper meaning is called?
+                            a) An allegory
+                            b) An elegy
+                            c) An epic
+                            d) A farce''',
+                         '''A poetic device which exaggerate situations or objects is known as?
+                            a) Exaggeration
+                            b) Romance
+                            c) Hyperbole
+                            d) Pun ''',
+                         '''The period of laughter or amusement in a tragic play is known as?
+                            a) Comic Relief
+                            b) Comedy Relief
+                            c) Climax
+                            d) Paradox ''',
+                         '''The first eight lines of a sonnet is called?
+                            a) A couplet
+                            b) An octave
+                            c) A quatrain
+                            d) A sestet ''',
+                         '''The last part of a literary work is known as?
+                            a) Acknowledgement
+                            b) Epilogue
+                            c) Prologue
+                            d) Conclusion ''',
+                         '''The identical sound at the end of a poem is known as?
+                            a) Mere
+                            b) Refrain
+                            c) Rhyme
+                            d) Rhythm ''',
+                         '''Community development projects can be best achieved if the people are?
+                            a) Cooperative
+                            b) Religious
+                            c) Optimistic
+                            d) Knowledgeable ''',
+                        ''' The civic obligation of a citizen includes
+                            a) Political socialization
+                            b) Political participation
+                            c) Engaging in family planning
+                            d) Engaging in business ventures''',
+                         '''HIV/AIDS is spread mainly through
+                            a) Indiscriminate sexual intercourse
+                            b) Kissing and hugging
+                            c) Sharing toiletries with victims
+                            d) Handshake with victims ''',
+                         ''' People living with HIV/AIDS could be assisted by
+                             a) Isolating them
+                             b) Showing them love
+                             c) Blaming them
+                             d) Avoiding them ''',
+                        ''' Which of the following does not promote national consciousness?
+                            a) Respect for the symbols and identities of the country
+                            b) Promotion of people’s culture and tradition
+                            c) Appropriate use of the mass media
+                            d) Inappropriate use of state property ''',
+                        ''' The habit of obeying traffic regulations helps the individual to
+                            a) Report late at work
+                            b) Avoid unnecessary accident
+                            c) Be lazy at work
+                            d) Use unauthorized route''',
+                        ''' Which of the following roles should a responsible parent perform?
+                            a) Sending the children to hawk on the street
+                            b) Instilling in children appropriate values and attitude
+                            c) Encouraging early marriage
+                            d) Showing favour to a particular child '''
+                        }
+arts_answers = {
+                     'Antagonist',
+                     'Simile',
+                     'An allegory',
+                     'Hyperbole',
+                     'Comic relief',
+                     'Octave',
+                     'Epilogue',
+                     'Rhyme',
+                     'Cooperative',
+                     'Political participation',
+                     'Indiscriminate sexual intercourse',
+                     'Showing them love',
+                     'Inappropriate use of state property',
+                     'Avoid unnecessary accident',
+                     'Instilling in children appropriate values and attitude'
+            }
+
+
+                     
+                     
+                     
+                     
+                     
+                     
+            
+                     
+                    
+
+                 
+        
+                 
