68966dce0723cfd0a747036b086389f65771165a
peterkabaijr <56918289+peterkabaijr@users.noreply.github.com>
2019-11-27 04:16:48 -0600

    Add files via upload

---
+++ GROUP PROJECT2 --ART.py
@@ -0,0 +1,20 @@
+Art_questions={'the system of govt whereby the powers and authorities are controlled by a single central govt is called what?',
+               'the process by which the govt of a certain country is controlled by priests and religious is called what?',
+               'the father of nigeria nationalism is who?'
+               'the military govt of Nigeria ruled the country using what system of government?',
+               'the first executive president of Nigeria was who?',
+               'the first four disciples Jesus called were who?',
+               'the first martyr of the church is who?',
+               'who did God send to wage war on the Amalekites but disobeyed the further instructions given by Him?'
+               }
+Art-answers={'Unitary system of govt',
+             'Theocracy',
+             'Herbert Macaulay',
+             'Autocracy',
+             'Alhaji Shehu Shagari',
+             'Peter,Andrew,James and John',
+             'Stephen',
+             'King Saul'
+             }
+               
+              
