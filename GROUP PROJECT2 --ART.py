b18b0e101cbc3a7602c3732e8e1ad32ff7af0d3b
peterkabaijr <56918289+peterkabaijr@users.noreply.github.com>
2019-11-27 05:42:21 -0600

    Add files via upload

--- GROUP PROJECT2 --ART.py
+++ GROUP PROJECT2 --ART.py
@@ -1,20 +1,56 @@
-Art_questions={'the system of govt whereby the powers and authorities are controlled by a single central govt is called what?',
-               'the process by which the govt of a certain country is controlled by priests and religious is called what?',
-               'the father of nigeria nationalism is who?'
-               'the military govt of Nigeria ruled the country using what system of government?',
-               'the first executive president of Nigeria was who?',
-               'the first four disciples Jesus called were who?',
-               'the first martyr of the church is who?',
-               'who did God send to wage war on the Amalekites but disobeyed the further instructions given by Him?'
+Art_questions={'''The system of govt whereby the powers and authorities are controlled by a single central govt is called what?
+                 a)Unitary system
+                 b)Federal system
+                 c)Confederal system
+                 d)presidential system''',
+               '''Which of the following countries practised mixed economy?
+                 a)USA
+                 b)Britain
+                 c)China
+                 d)Nigeria''',
+               '''Theocracy is a govt ruled by who?
+                 a)Nobles
+                 b)Priests and religious
+                 c)Old people
+                 d)The military''',
+               '''The Father of Nigeria Nationalism is who?
+                 a)Nnamdi Azikiwe
+                 b)Obafemi Awolowo
+                 c)Herbert Macaulay
+                 d)Ahmadu Bello''',
+               '''The first executive president of Nigeria was who?
+                 a)Gen.Aguiyi Ironsi
+                 b)Nnamdi Azikiwe
+                 c)Alhaji Shehu Shagari
+                 d)Olusegun Obasanjo''',
+               '''The sons of King Saul who were murdered during the battle against the Philistines were who?
+                 a)Peter,James and John
+                 b)Jonathan,Abinadab and Malchishua
+                 c)Shem,Ham and Japhat
+                 d)Joab,Abishai and Ammon''',
+               '''The king of Judah who set himself ablaze in his palace and died was who?
+                 a)King Ahab
+                 b)King David
+                 c)King Zimri
+                 d)King Cyrus''',
+               '''The prophet who is known for true religion and social justice was who?
+                 a)Prophet Elijah
+                 b)Prophet Micah
+                 c)Prophet Hosea
+                 d)Prophet Amos'''
                }
-Art-answers={'Unitary system of govt',
-             'Theocracy',
-             'Herbert Macaulay',
-             'Autocracy',
-             'Alhaji Shehu Shagari',
-             'Peter,Andrew,James and John',
-             'Stephen',
-             'King Saul'
-             }
+
+
+
+Answers={'a)unitary system',
+         'd)nigeria',
+         'b)priests and religious',
+         'c)Herbert Macaulay',
+         'c)Alhaji Shehu Shagari',
+         'b)Jonathan,Abinadab and Malchishua',
+         'c)King Zimri',
+         'd)Prophet Amos',
+         }
+         
+
                
-              
